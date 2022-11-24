70412eec06cf1450474eb442d1e1e5e06b0ed205
sohail3031 <mohdsohailahmed46@gmail.com>
2022-11-24 19:39:47 +0530

    first commit

---
+++ errors/index.js
@@ -0,0 +1,5 @@
+const CustomAPIError = require("./custom-error");
+const BadRequestError = require("./bad-request");
+const UnauthenticatedError = require("./unauthentication");
+
+module.exports = { CustomAPIError, BadRequestError, UnauthenticatedError };
